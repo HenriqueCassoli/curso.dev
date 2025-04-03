b7df28a0ca9a448135bdb0e5c78bf10333fc8b56
Henrique Fernandes Cassoli <80260069+HenriqueCassoli@users.noreply.github.com>
2025-04-03 14:22:40 +0000

    adiciona scripts com ´lint:check´

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home() {
-    return <h1>  [Letra de "Clube Tião Caminhoneiro Hell" com U.D.R]</h1> ;
-
+  return <h1> [Letra de "Clube Tião Caminhoneiro Hell" com U.D.R]</h1>;
 }
 
+function teste() {
+  console.log("indentação errado");
+}
 export default Home;
